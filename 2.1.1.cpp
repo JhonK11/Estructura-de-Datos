227930d645150c57b127e9ad12bcf88ff6e69df7
Jhon Chambi <chambijhon23@gmail.com>
2025-06-04 18:25:17 -0500

    Add files via upload

---
+++ 2.1.1.cpp
@@ -0,0 +1,27 @@
+// C2:p1 2.1.1
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a, b;
+    int c,d;
+    string m;
+    string x;
+    cin >> a >> b>> x;
+    cout << a<<" "<< b<<" "<<x<<endl;
+
+    scanf("%d %d", &c, &d);
+    printf("%d %d\n", c, d);
+
+    getline(cin , m);
+    cout << m;
+
+    string p;
+    while(cin >> p){
+        cout << p << "\n";
+    }
+    return 0;
+}
+
+
+
+
---
+++ 2.2.2segundo.cpp
@@ -0,0 +1,17 @@
+//C2:P3 2.2.2
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> permutation;
+    for (int i = 1; i <= n; i++) {
+        permutation.push_back(i);
+    }
+    do {
+        for (int x : permutation) cout << x << " ";
+        cout << "\n";
+    } while (next_permutation(permutation.begin(), permutation.end()));
+    return 0;
+}
